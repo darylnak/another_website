589826062a7f962533562b16a6a549e06db19b05
BuildTools <unconfigured@null.spigotmc.org>
2019-11-18 01:55:49 -0800

    mad animations

--- js/navigation.js
+++ js/navigation.js
@@ -8,3 +8,32 @@ function showLanding(){
     document.getElementById("content_landing").style.display = "block";
 
 }
+
+let isOn = true;
+const speed = 650;
+let i = 0;
+const txt = 'Made for humans';
+let randTypeTime = Math.floor(Math.random() + 40) + 30;
+
+window.onload = function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("slogan").innerHTML += txt.charAt(i);
+        i++;
+        randTypeTime = Math.floor(Math.random() * 80) + 60;
+        setTimeout(typeWriter, randTypeTime);
+    }
+}
+
+active = false;
+
+setInterval(() => {
+    if(isOn && document.getElementById("slogan").textContent.localeCompare(txt) == 0){
+        document.getElementById("blinker").style.opacity = 0;
+        isOn = false;
+    }
+    else{
+        document.getElementById("blinker").style.opacity = 1;
+        isOn = true;
+    }
+}, speed);
+
